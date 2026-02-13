df0285a168fc3fb4d2f5b160a99b075897559ab6
Pete Kayuha <zzz8910@gmail.com>
2026-02-13 15:18:26 -0500

    End of section 6 - routing

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,44 +1,22 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
-import { User } from "../types/user";
+import { Router, RouterOutlet } from "@angular/router";
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected readonly title = signal('Dating App');
-  protected members = signal<User[]>([]);
+export class App {
+  protected router = inject(Router);
 
-  async ngOnInit() {    
-      this.members.set(await this.getMembers());
-      this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-
-    if(!userString)
-      return;
-
-    const user = JSON.parse(userString)
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get<User[]>('http://localhost:5000/api/members'));
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
 }
+
+/**
+ * angular 20+ style for routing
+ * not using due to it not using full screen. sides are in
+ *     [class.container]="router.url ! == '/'"
+    [class.mx-auto]="router.url ! == '/'"
+ */
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,28 +1,37 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
-  protected creds: any = {};
+  private router = inject(Router);
+  private toast = inject(ToastService);
+  protected creds: any = {};  
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: (result) => {
-        console.log(result);
+      next: () => {
+        this.router.navigateByUrl('/members');
+        this.toast.success('Login successful');
         this.creds = {};
       },
-      error: (error) => alert(error.message),
+      error: (error) => {
+        console.log(error);
+        this.toast.error(error.error);
+      }
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
